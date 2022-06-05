07539ca6a930037f8c794723960fce68bbe20c8a
SteveenHuawei <ss.rodriguez00021@gmail.com>
2022-06-05 18:16:48 -0500

    creación del método getPokemon, trae todos datos

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  private url: string = "https://pokeapi.co/api/v2/pokemon";
+
+  constructor(private http : HttpClient) { }
+
+  getPokemon(name : string){
+    return this.http.get(`${this.url}/${name}`)
+  }
 }
